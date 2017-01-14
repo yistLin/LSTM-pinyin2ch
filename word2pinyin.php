da2a32beb6e0f947e742c1cae55c6d83e6fbebbe
Yist Lin <yishen992@gmail.com>
2017-01-14 22:57:12 +0800

    fix 0 disappearing problem

--- word2pinyin.php
+++ word2pinyin.php
@@ -8,11 +8,12 @@ $file = fopen($argv[1], 'r');
 
 while (!feof($file)) {
     $line = trim(fgets($file));
-    $arr = $pinyin->convert($line);
-    echo $line . "\t" . implode(' ', $arr) . "\n";
+    $pline = $pinyin->sentence($line);
+    echo $line . "\t" . $pline . "\n";
 }
 
-// echo implode(' ', $pinyin->convert('带着希望去旅行，比到达终点更美好')) . "\n";
+// echo implode(' ', $pinyin->convert('带着希望去旅行，比到达终点更美好 2 0 世紀')) . "\n";
+// echo $pinyin->sentence('带着希望去旅行，比到达终点更美好 2 0 世紀') . "\n";
 
 fclose($file);
 ?>
