7f718b0227c8f3e23736163d115c06aeb1e5bdf2
b03902048 <b03902048@csie.ntu.edu.tw>
2017-01-15 13:54:03 +0800

    rearrange the directory structure

---
+++ crawler/makeList/seq.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if len(sys.argv) != 2:
+  print("Usage: ./seq.py [filename]")
+  sys.exit(1)
+
+int 
--- makeList/seq.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-if len(sys.argv) != 2:
-  print("Usage: ./seq.py [filename]")
-  sys.exit(1)
-
-int 
