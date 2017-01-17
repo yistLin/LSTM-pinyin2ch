e971fd3a96c06faae99da44efe356ac2ad9a38fa
WindQAQ <b03902042@ntu.edu.tw>
2017-01-18 01:03:57 +0800

    add embedding_rnn_seq2seq and modify batch format

--- model.py
+++ model.py
@@ -2,22 +2,56 @@ import tensorflow as tf
 
 class Model():
 
-    def __init__(self, rnn_size, seq_len, batch_size):
+    def __init__(self, rnn_size, seq_len, batch_size, source_vocab_size, target_vocab_size):
         self.__rnn_size = rnn_size
         self.__seq_len = seq_len
         self.__batch_size = batch_size
+        self.__source_vocab_size = source_vocab_size
+        self.__target_vocab_size = target_vocab_size
 
     def build_graph(self):
         print('[Build graph]')
         rnn_size = self.__rnn_size
         seq_len = self.__seq_len
         batch_size = self.__batch_size
+        source_vocab_size = self.__source_vocab_size
+        target_vocab_size = self.__target_vocab_size
         print('\trnn size: {}'.format(rnn_size))
         print('\tseq len: {}'.format(seq_len))
         print('\tbatch_size: {}'.format(batch_size))
-        self.__graph = tf.Graph()
-        with self.__graph.as_default():
-            print('[RNN cell]: create')
-            self.__rnn_cell = rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_size)
-            print('[State]: create')
-            self.__state = state = tf.zeros([batch_size, rnn_cell.state_size])
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.encode_inputs = [tf.placeholder(tf.int32, shape=(batch_size,)) for _ in range(seq_len)]
+            self.decode_inputs = [tf.placeholder(tf.int32, shape=(batch_size,)) for _ in range(seq_len)]
+            self.targets = [tf.placeholder(tf.int32, shape=(batch_size,)) for _ in range(seq_len)]
+            #self.encode_inputs = tf.placeholder(tf.int32, shape=[batch_size, seq_len])
+            #self.decode_inputs = tf.placeholder(tf.int32, shape=[batch_size, seq_len])
+            #self.labels = tf.placeholder(tf.float32, shape=[batch_size, seq_len])
+
+            #print('[Placeholder]')
+            #print('\tencode inputs shape: {}'.format(self.encode_inputs.get_shape()))
+            #print('\tdecode inputs shape: {}'.format(self.decode_inputs.get_shape()))
+            #print('\tlabels shape: {}'.format(self.labels.get_shape()))
+
+            self.__rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_size)
+            print('[RNN cell]')
+            print('\toutput size: {}'.format(self.__rnn_cell.output_size))
+            print('\tstate size: {}'.format(self.__rnn_cell.state_size))
+
+            #self.split_inputs = tf.split(1, seq_len, self.encode_inputs)
+            #print('[Split input]')
+            #print('\tsplit input: {}'.format(self.split_inputs))
+            # self.__state = tf.zeros([batch_size, self.__rnn_cell.state_size])
+
+            self.outputs, self.__state = tf.nn.seq2seq.embedding_rnn_seq2seq(
+                    self.encode_inputs, self.decode_inputs, self.__rnn_cell,
+                    source_vocab_size, target_vocab_size, 256)
+            print('[Output]')
+            print('\toutputs: ', self.outputs)
+
+            weights = [tf.constant(1.0, shape=[batch_size]) for _ in range(seq_len)]
+            self.loss = tf.nn.seq2seq.sequence_loss(self.outputs, self.targets, weights)
+
+            self.optimizer = tf.train.AdamOptimizer().minimize(self.loss)
+
+            self.init_op = tf.global_variables_initializer()
--- train.py
+++ train.py
@@ -15,19 +15,21 @@ class BatchGenerator():
         source_PADID = self.__source_map.PADID
         target_PADID = self.__target_map.PADID
         with open(self.filename, 'r') as f:
-            batch = []
+            batch_source = []
+            batch_target = []
             for (i, line) in enumerate(f):
                 line = line.strip().split('\t')
                 source, target = line[0].strip().split(), line[1].strip().split()
                 source_pad_len = self.seq_len - len(source)
                 target_pad_len = self.seq_len - len(target)
-                batch.append((
-                                list(map(lambda x: self.__source_map.word2id[x], source)) + [source_PADID] * source_pad_len, 
-                                list(map(lambda x: self.__target_map.word2id[x], target)) + [target_PADID] * target_pad_len
-                            ))
-                if len(batch) == self.batch_size:
-                    yield list(zip(*batch))
-                    batch.clear()
+                source_id_list = list(map(lambda x: self.__source_map.word2id[x], source))
+                target_id_list = list(map(lambda x: self.__target_map.word2id[x], target))
+                batch_source.append(source_id_list + [source_PADID] * source_pad_len)
+                batch_target.append(target_id_list + [target_PADID] * target_pad_len)
+                if len(batch_source) == self.batch_size:
+                    yield (list(zip(*batch_source)), list(zip(*batch_target)))
+                    batch_source.clear()
+                    batch_target.clear()
 
 class Map():
     def __init__(self, words):
@@ -39,7 +41,7 @@ class Map():
         self.__build_map()
 
     def __build_map(self):
-        self.__words += ['_PAD'] + ['_UNK']
+        self.__words += ['_PAD'] + ['_UNK'] + ['_GO'] + ['_EOS']
         counter = Counter(self.__words)
         self.id2word = sorted(counter, key=counter.get, reverse=True)
         self.word2id = {y: x for (x, y) in enumerate(self.id2word)}
@@ -61,13 +63,34 @@ def preprocess(filename):
     return source_map, target_map
     
 def train(arg):
-    source_map, target_map = preprocess(arg.train_data)
-    train_batches = BatchGenerator(arg.train_data, arg.batch_size, arg.seq_len, source_map, target_map)
-    #for batch in train_batches.next():
-    #    print(batch)
+    train_data = arg.train_data
+    rnn_size = arg.rnn_size
+    seq_len = arg.seq_len
+    batch_size = arg.batch_size
+    num_epoch = arg.num_epoch   
+ 
+    source_map, target_map = preprocess(train_data)
     
-    seq2seqModel = Model(arg.rnn_size, arg.seq_len, arg.batch_size)
+    seq2seqModel = Model(rnn_size, seq_len, batch_size, source_map.size, target_map.size)
     seq2seqModel.build_graph()
+    with tf.Session(graph=seq2seqModel.graph) as sess:
+        sess.run(seq2seqModel.init_op)
+        for epoch in range(arg.num_epoch):
+            train_batches = BatchGenerator(train_data, batch_size, 
+                    seq_len, source_map, target_map)
+            feed_dict = {}
+            for batch in train_batches.next():
+                    for i in range(seq_len):
+                        feed_dict[seq2seqModel.encode_inputs[i]] = batch[0][i]
+                        feed_dict[seq2seqModel.decode_inputs[i]] = batch[1][i]
+                        feed_dict[seq2seqModel.targets[i]] = batch[1][i]
+
+                    _, loss = sess.run([seq2seqModel.optimizer, seq2seqModel.loss], feed_dict=feed_dict)
+                    outputs = sess.run([seq2seqModel.outputs], feed_dict=feed_dict)
+                    print('loss: {:.10f}'.format(loss))
+                    print('targets: {}'.format(batch[1]))
+                    print('outputs: {}'.format(outputs))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train seq2seq model.')
@@ -75,4 +98,5 @@ if __name__ == '__main__':
     parser.add_argument('-batch_size', action='store', dest='batch_size', type=int, required=True)
     parser.add_argument('-rnn_size', action='store', dest='rnn_size', type=int, required= True)
     parser.add_argument('-seq_len', action='store', dest='seq_len', type=int, required=True)
+    parser.add_argument('-num_epoch', action='store', dest='num_epoch', type=int, required=True)
     train(parser.parse_args())
